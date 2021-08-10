9c0fcb35b1ed67439650979ba39f7fe70ed7397b
amir <amirhossein.maleki1990@gmail.com>
2021-08-10 19:13:50 +0430

    Second Version-August 10th

---
+++ dataset.py
@@ -0,0 +1,235 @@
+import os
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+
+import neurokit2 as nk
+import scipy.signal as signal
+
+import warnings
+
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
+
+
+
+
+class CASE:
+
+    def __init__(self, phy_dir, ann_dir, arch='CENT'):
+        print('\nBuilding the DATASET. ')
+
+        self.phy_add = CASE.read(phy_dir, '*.csv')
+        self.ann_add = CASE.read(ann_dir, '*.csv')
+        self.NumberOfUsers = len(self.phy_add)
+        self.arch = arch
+        self.x, self.y, self.cwt = [], [], []
+
+
+    @staticmethod
+    def read(pathdir, f_extention):
+        return np.array(tf.io.gfile.glob(str(pathdir) + f_extention))
+
+    @staticmethod
+    def dataframe(address, columns):
+        return pd.read_csv(address, usecols=columns, index_col=False)
+
+    #
+    @staticmethod
+    def mapping(row):
+        """Mapping function helps us map the label to several predifined classes"""
+
+        if row['arousal'] >= 0 and row["valence"] >= 0:  # HIGH arousal - HIGH valence (HH)
+            return [1, 1]
+
+        elif row['arousal'] >= 0 and row["valence"] < 0:  # HIGH arousal - LOW valence (HL)
+            return [1, 0]
+
+        elif row['arousal'] < 0 and row["valence"] >= 0:  # LOW arousal - HIGH valence (LH)
+            return [0, 1]
+
+        elif row['arousal'] < 0 and row["valence"] < 0:  # LOW arousal - LOW valence (LL)
+            return [0, 0]
+
+    @staticmethod
+    def minmax__norm(raw_signal):
+        return ((raw_signal - raw_signal.min()) / (raw_signal.max() - raw_signal.min()))
+
+    @staticmethod
+    def decompose(raws_signal):
+        sig, info = nk.eda_process(raws_signal, sampling_rate=1000)
+        return sig['EDA_Phasic']
+        # sig['EDA_Phasic'] sig['EDA_Tonic'] sig['SCR_Peaks'] sig['SCR_RiseTime']
+
+    @staticmethod
+    def feature_extraction(sample):
+        widths = np.arange(1, 21)
+        return signal.cwt(sample['gsr_phasic'], signal.ricker, widths=widths).T  # shape (20, 500)
+
+
+    @staticmethod
+    def grouped_by_video_id(df, feature='video'):
+        grouped_by_video_id = df.groupby(feature)
+        df_list_grouped_by_video_id = []
+        for grouped_df in grouped_by_video_id:
+            # print('gp by type ', type(grouped_df[1))
+            df_list_grouped_by_video_id.append(grouped_df[1])
+        # a list of tuples including (video id, data frame grouped by video id)
+        return np.array(df_list_grouped_by_video_id) # it returns a numpy array of pandas data frames grouped by 'VIDEO ID
+
+
+    ###############################################################################################################
+    ###############################################################################################################
+
+    #                       CENT
+
+    ###############################################################################################################
+    ###############################################################################################################
+
+    @staticmethod
+    def session_chunk(gp_sess_ann_df, gp_sess_phy_df):
+        temp_x, temp_y, temp_cwt = [], [], []
+        str_p = gp_sess_ann_df['jstime'].iloc[0]
+        #
+        for k in range(20, gp_sess_ann_df.shape[0], 20):
+            end_p = gp_sess_ann_df['jstime'].iloc[k]
+            label = gp_sess_ann_df[(gp_sess_ann_df['jstime'] <= end_p) & (gp_sess_ann_df['jstime'] > str_p)]
+            valence = label['valence'].diff().mean()
+            arousal = label['arousal'].diff().mean()
+            sample = gp_sess_phy_df[(gp_sess_phy_df['daqtime'] <= end_p) & (gp_sess_phy_df['daqtime'] > str_p)]
+
+            clss = CASE.mapping(row={'arousal': arousal, 'valence': valence})
+            cwt = CASE.feature_extraction(sample=sample)
+            str_p = gp_sess_ann_df['jstime'].iloc[k]
+
+            if sample.shape[0] == 1000:
+                temp_x.append(sample['gsr_phasic'].to_numpy())
+                temp_y.append(np.array(clss))
+                temp_cwt.append(cwt)
+
+        return np.array(temp_x), np.array(temp_y), np.array(temp_cwt)
+
+
+    def process(self, usr):
+
+        phy_col = ['daqtime', 'gsr', 'video']
+        ann_col = ['jstime', 'valence', 'arousal', 'video']
+
+        username = self.phy_add[usr].split('/')[-1].split('.')[0]
+        tusrname = self.ann_add[usr].split('/')[-1].split('.')[0]
+        assert username == tusrname
+
+        self.phy_df = CASE.dataframe(self.phy_add[usr], phy_col)
+        self.ann_df = CASE.dataframe(self.ann_add[usr], ann_col)
+
+        # min max normalization and decomposition are applied on each person's GSR signal
+        self.phy_df['gsr'] = CASE.minmax__norm(self.phy_df['gsr'])
+        self.phy_df['gsr_phasic'] = CASE.decompose(self.phy_df['gsr'])
+
+        gp_phy_df = self.grouped_by_video_id(self.phy_df, feature='video')
+        gp_ann_df = self.grouped_by_video_id(self.ann_df, feature='video')
+
+        return gp_phy_df, gp_ann_df
+
+
+    def cent_process(self):
+
+        for usr in range(self.NumberOfUsers):
+            # for i in range(5):
+
+            gp_phy_df, gp_ann_df = self.process(usr=usr)
+
+            for sess in range(gp_ann_df.shape[0]):
+                gp_sess_ann_df = gp_ann_df[sess]
+                gp_sess_phy_df = gp_phy_df[sess]
+                # gp_sess_phy_df = phy.get_group(ann[sess][0])
+                x, y, cwt = CASE.session_chunk(gp_sess_ann_df=gp_sess_ann_df, gp_sess_phy_df=gp_sess_phy_df)
+                self.x.append(x), self.y.append(y), self.cwt.append(cwt)
+
+            self.x = np.array(self.x)
+            self.y = np.array(self.y)
+            self.cwt = np.array(self.cwt)
+
+    ###############################################################################################################
+    ###############################################################################################################
+
+    #                       FED
+
+    ###############################################################################################################
+    ###############################################################################################################
+
+
+    def fed_process(self):
+        print('making the DATASET federated')
+
+        # for i in range(self.NumberOfUsers):
+        for usr in range(10):
+
+            gp_phy_df, gp_ann_df = self.process(usr=usr)
+            sess_x, sess_y, sess_cwt = [], [], []
+
+            for sess in range(gp_ann_df.shape[0]):
+                gp_sess_ann_df = gp_ann_df[sess]
+                gp_sess_phy_df = gp_phy_df[sess]
+                # gp_sess_phy_df = phy.get_group(ann[sess][0])
+                x, y, cwt = CASE.session_chunk(gp_sess_ann_df=gp_sess_ann_df, gp_sess_phy_df=gp_sess_phy_df)
+                sess_x.append(x), sess_y.append(y), sess_cwt.append(cwt)
+
+            self.x.append(np.array(sess_x))
+            self.y.append(np.array(sess_y))
+            self.cwt.append(np.array(sess_cwt))
+
+        self.x = np.array(self.x)
+        self.y = np.array(self.y)
+        self.cwt = np.array(self.cwt)
+
+
+
+    def save_dataset(self):
+        start_time = datetime.now()
+        if self.arch == 'CENT':
+            print('\n Centralized Architecture DATASET')
+            if not os.path.isdir('./dataset/CENT/'):
+                os.makedirs('./dataset/CENT/', exist_ok=True)
+            self.cent_process()
+            print('building DATASET duration time ', datetime.now()-start_time)
+
+        elif self.arch == 'FED':
+            print('\n Federated Architecture DATASET')
+            if not os.path.isdir('./dataset/FED/'):
+                os.makedirs('./dataset/FED/', exist_ok=True)
+            self.fed_process()
+            print('building DATASET duration time ', datetime.now()-start_time)
+
+            # self.fed_process()
+
+        print(self.x.shape, self.x.shape, self.cwt.shape)
+        np.save('./dataset/{}/x.npy'.format(self.arch), np.array(self.x))  # save
+        np.save('./dataset/{}/y.npy'.format(self.arch), np.array(self.y))  # save
+        np.save('./dataset/{}/cwt.npy'.format(self.arch), np.array(self.cwt))  # save
+
+        # print('dataset is saved in "x.npy", "y.npy", and cwt.npy files')
+
+    def load_data(self, x='/x.npy', y='/y.npy', cwt='/cwt.npy'):
+        print('Loading the Dataset. ')
+        wd = './dataset/'
+        if not os.path.isdir(wd):
+            os.makedirs(wd, exist_ok=True)
+        if not os.path.isfile(wd+'{}/y.npy'.format(self.arch)):
+            self.save_dataset()
+
+
+        x = np.load('./dataset/{}/x.npy'.format(self.arch), allow_pickle=True)
+        y = np.load('./dataset/{}/y.npy'.format(self.arch), allow_pickle=True)
+        cwt = np.load('./dataset/{}/cwt.npy'.format(self.arch), allow_pickle=True)
+
+        if self.arch == 'CENT':
+            x = np.concatenate(x, np.load('./dataset/CENT/x1.npy', allow_pickle=True), axis=0)
+            y = np.concatenate(y, np.load('./dataset/CENT/y1.npy', allow_pickle=True), axis=0)
+            cwt = np.concatenate(cwt, np.load('./dataset/CENT/cwt1.npy', allow_pickle=True), axis=0)
+
+
+        return x, y, cwt
+
---
+++ main.py
@@ -0,0 +1,271 @@
+import os
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+import dataset as dt
+
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Input, Conv1D, Conv2D, MaxPooling2D, Dropout
+from tensorflow.keras.layers import Flatten, Dense, BatchNormalization, Concatenate
+from tensorflow.keras.models import Model
+import matplotlib.pyplot as plt
+
+pd.options.display.width = 0
+
+
+
+class DNN:
+
+  def __init__(self, kwargs):
+
+    print('The DNN class is building ....')
+
+    # initializing values
+    self.phy_dir = kwargs.get('phy_dir', './')
+    self.ann_dir = kwargs.get('ann_dir', './')
+    self.arch = kwargs.get('architecture', 'CENT')
+    self.ml = kwargs.get('model', 'CNN').upper()
+    self.decompose_flag = kwargs.get('decompose', False)
+    self.gsr_only_flag = kwargs.get('gsr_only', False)
+    self.minmax = kwargs.get('minmax_norm', False)
+    self.ecg_flag = kwargs.get('ecg', False)
+    self.lr = kwargs.get('lr', 0.0001)
+    self.optimizer = kwargs.get('optimizer', Adam(self.lr))
+    "TODO: WE CAN CONSIDER OTHER FEATURES SAME AS THE ABOVE FEATURES WITH SAME METHOD"
+
+
+    assert self.ml == 'CNN' or self.ml == 'LSTM'
+    print('In this run, we use a {}-based model with a {} architecture'.format(self.ml, self.arch))
+
+    # Dataset class is called and built here.
+    # The load_data() function gives us Phasic GSR (x),
+    # features extracted by Continuous wavelet transform (cwt) from Phasic GSR,
+    # and labels (y)
+    dataset = dt.CASE(self.phy_dir, self.ann_dir, self.arch)
+    self.x, self.y, self.cwt = dataset.load_data()
+    print('The Dataset is loaded.')
+    # print('Raw GSR shape: {},  Label shape: {}, cwt shape: {}'.format(self.x.shape, self.y.shape, self.cwt.shape))
+
+
+    # self.NUM_USRS is the number of file in the dataset
+    self.Num_Usr = self.x.shape[0]
+    self.C = kwargs.get('C', self.x.shape[0])
+    self.Num_Sess = kwargs.get('Num_Sess', self.x.shape[1])
+
+
+
+
+
+    if self.arch == 'CENT':
+      print('\nThe number of all stacked samples are', self.x.shape[0])
+      self.model = self._create_model()
+
+    elif self.arch == 'FED':
+      print('\nThe Number of Users is {}, The Number of sessions for each user is {}.'.format(self.Num_Usr,
+                                                                                              self.Num_Sess))
+      print('The Number of Users used for aggregation in the global model (C) is {} '.format(self.C))
+      # self.model = [self._create_model() for num_usr in range((self.Num_Usr-1)]
+      self.model = [self._create_model() for num_usr in range(3)] # this line will be deleted
+
+      print('Number of models used for aggregating the global model is  {}.'.format(len(self.model)))
+      self.global_model = self._create_model()
+
+
+
+  def _create_model(self):
+
+    if self.gsr_only_flag:
+      input_1 = Input(shape=(1000, 1), name="input_1")
+      # input_2 = Input(shape=(50, 2,  1), name="input_2")
+      input_2 = Input(shape=(1000, 20, 1), name="input_2")
+    else:
+      'TODO: it should be determinded how many features we are going to work on.'
+      pass
+
+    if self.ml == 'CNN':
+      output1, output2 = self._cnn_model([input_1, input_2])
+    elif self.ml == 'LSTM':
+      'TODO: We should compelte the LSTM model'
+      pass
+
+
+    losses = {'arousal': 'binary_crossentropy',
+              'valence': 'binary_crossentropy',}
+
+    metrics = {'arousal': 'accuracy',
+               'valence': 'accuracy',}
+
+    model = Model(inputs=[input_1, input_2], outputs=[output1, output2],)
+    # plot_model(self.cnn, to_file='CNN.png', show_shapes=True, show_layer_names=True)
+    model.compile(optimizer=self.optimizer,
+                  loss=losses,
+                  metrics=metrics)
+
+
+    return model
+
+
+  def _cnn_model(self, _input):
+
+    # print('\n    building    **** CNN ****   \n\n ')
+    # print('shapes are ')
+    # print(_input[0].shape, _input[1].shape, '_input[2].shape')
+
+    l1 = Conv1D(32, kernel_size=(5), padding='same', strides=1, activation='relu')(_input[0])
+    # l2 = Conv1D(64, kernel_size=(5), padding='same', strides=1, activation='relu')(l1)
+    # print('first input ', l1.shape, l2.shape)
+
+    l3 = Conv2D(16, kernel_size=5, padding='same', strides=1, activation='relu')(_input[1])
+    # MaxPooling2D()
+    # l4 = Conv2D(32, kernel_size=5, padding='same', strides=1, activation='relu')(l3)
+    # print('second input ', l3.shape, l4.shape)
+
+    # l5 = Conv2D(16, kernel_size=5, padding='same', strides=1, activation='relu')(_input[2])
+    # l6 = Conv2D(32, kernel_size=5, padding='same', strides=1, activation='relu')(_input[2])
+    # print('third input ', l5.shape, l6.shape)
+
+    # l3 = Conv1D(64, kernel_size=(5), padding='same', strides=2, activation='relu')(l2)
+    # print(l3.shape)
+    #
+    # l4 = Conv1D(128, kernel_size=(5), padding='same', strides=2, activation='relu')(l3)
+    # print(l4.shape)
+    #
+    # l5= Conv1D(128, kernel_size=(5), padding='same', strides=2, activation='relu')(l4)
+    # print(l5.shape)
+    l1 = Flatten()(l1)
+    l3 = Flatten()(l3)
+    # l6 = Flatten()(l6)
+    # print('flattened ', l1.shape, l3.shape, 'l6.shape')
+    conc_l = Concatenate()([l1, l3])
+
+
+    l7 = Dense(units=128, activation='relu')(conc_l)
+    l8 = Dense(units=64, activation='relu')(l7)
+
+    out1 = Dense(units=1, activation='sigmoid', name='arousal')(l8)
+    out2 = Dense(units=1, activation='sigmoid', name='valence')(l8)
+
+    return out1, out2
+
+  def LSTM(self):
+    'TODO: We are going to design an LSTM model.'
+    pass
+
+  def RNN(self):
+    pass
+
+  @staticmethod
+  def get_average_weights(models):
+
+    weights = [model.get_weights() for model in models]
+    new_weights = list()
+
+    for weights_list_tuple in zip(*weights):
+      new_weights.append(
+        np.array([np.array(w).mean(axis=0) for w in zip(*weights_list_tuple)]))
+
+    return new_weights
+
+  def stack_up(self, ith_usr):
+    """
+
+    This function stacks up all the sessions array in the I-th user in one sequential array.
+    It is just used in FED architecture
+    If we do this function, it means our framework is using a IID format of a dataset.
+
+    :param ith_usr: The number of user's place in the row of 30 users in the dataset
+    :return: return sequential expanded-dimension array of x, y, cwt
+
+    """
+
+    x = np.concatenate([self.x[ith_usr][i] for i in range(self.Num_Sess)])
+    y = np.concatenate([self.y[ith_usr][i] for i in range(self.Num_Sess)])
+    cwt = np.concatenate([self.cwt[ith_usr][i] for i in range(self.Num_Sess)])
+
+    return np.expand_dims(x, axis=-1), np.expand_dims(y, axis=-1), np.expand_dims(cwt, axis=-1)
+
+
+  def train(self, B=32, GE=10, LE=1):
+    """
+    This function Trains the model.
+
+    :param B: B represents the number of sample in a batch for each iteration
+    :param GE: GE is showing either Global Epochs in FED architecture or
+               the number of Epochs in CENT architecture.
+    :param LE: The number of Local Epochs each client makes over its local dataset.
+               It is used just in the FED architecture.
+    :return: None
+
+    """
+    print('\nTraining Phase is starting. It may take a while to train the model.')
+
+    if self.arch == 'CENT':
+
+      self.x = np.expand_dims(self.x, axis=-1)
+      self.y = np.expand_dims(self.y, axis=-1)
+      self.cwt = np.expand_dims(self.cwt, axis=-1)
+
+      self.model.fit(x=[self.x, self.cwt], y={"arousal": self.y[:, 0], "valence": self.y[:, 1]},
+                     batch_size=B, epochs=GE, verbose=1)
+
+
+
+    elif self.arch == 'FED':
+      test_x, test_y, test_cwt = self.stack_up(self.Num_Usr-1)
+
+      for ge in range(GE):
+
+        _ = [model.set_weights(self.global_model.get_weights()) for model in self.model]
+
+        for ith in range(len(self.model)):
+
+          x, y, cwt = self.stack_up(ith)
+          # print('shapes after def stack_up() ', x.shape, y.shape, cwt.shape)
+          self.model[ith].fit(x=[x, cwt], y={"arousal": y[:, 0], "valence": y[:, 1]},
+                              batch_size=B, epochs=LE, verbose=2)
+
+        rand_models_for_global_avg = np.random.choice(self.model, size=self.C, replace=False).tolist() # we can set p here to consider a weight for each mdoel
+
+        self.global_model.set_weights(self.get_average_weights(rand_models_for_global_avg))
+
+        print('\nGlobal Epoch {} .'.format(ge))
+        results = self.global_model.evaluate(x=[test_x, test_cwt],
+                                             y={"arousal": test_y[:, 0], "valence": test_y[:, 1]},
+                                             batch_size=B)
+
+
+
+
+
+if __name__ == '__main__':
+  print('Starting ... \n')
+
+  annotation_dir = '/Users/amir/Desktop/DR/GSR/CASE_dataset/CASE_dataset/interpolated/annotations/'  # the directory of annotations
+  physiological_dir = '/Users/amir/Desktop/DR/GSR/CASE_dataset/CASE_dataset/interpolated/physiological/'  # the directory fo physiological signals
+
+  # attr = {'phy_dir': physiological_dir,
+  #         'ann_dir': annotation_dir,
+  #         'gsr_only': True,
+  #         'decompose': True,
+  #         'minmax_norm': True,
+  #         'architecture': 'CENT',
+  #         'model': 'CNN',
+  #         'C': 2}
+
+
+  attr = {'phy_dir': physiological_dir,
+          'ann_dir': annotation_dir,
+          'gsr_only': True,
+          'decompose': True,
+          'minmax_norm': True,
+          'architecture': 'FED',
+          'model': 'CNN',
+          'C': 2}
+
+
+  obj = DNN(attr)
+  obj.train(GE=5, LE=3)
+
+
+
+
